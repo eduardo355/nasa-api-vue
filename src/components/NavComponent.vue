64bd81484a69486acb78cda1e213c85cac52a289
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-22 16:13:18 -0600

    refactor: Update NavComponent to include "Picture of the day" link

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,13 +1,16 @@
 <template>
   <nav class="flex justify-between p-4">
     <div class="flex items-center">
-      <img src="/nasa-logo.png" width="90" alt="" />
+      <img src="/nasa-logo.png" width="70" alt="" />
       <h1 class="text-2xl font-bold">NASA - API</h1>
     </div>
     <ul class="flex items-center space-x-4">
-      <!-- <li class="hover:underline">
-        <router-link to="/apod">Astronomy Picture of the Day</router-link>
-      </li> -->
+      <li
+        class="hover:bg-gray-200 px-4 py-2 rounded-xl text-lg font-semibold"
+        :class="pathName === 'apod' && 'bg-gray-200 px-4 py-2 rounded-xl'"
+      >
+        <router-link to="/apod">Picture of the day</router-link>
+      </li>
       <li
         class="hover:bg-gray-200 px-4 py-2 rounded-xl text-lg font-semibold"
         :class="pathName === 'marsrovers' && 'bg-gray-200 px-4 py-2 rounded-xl'"
