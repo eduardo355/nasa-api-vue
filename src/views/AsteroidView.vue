166c341ecbac4267eb30efd41d97ae333fe1df76
eduardo355 <ingeniero.eduardo355@gmail.com>
2024-10-22 16:14:01 -0600

    refactor: Update AsteroidView component to use CardAsteroidByIdComponent

--- src/views/AsteroidView.vue
+++ src/views/AsteroidView.vue
@@ -2,14 +2,14 @@
   <div class="xl:col-span-4 lg:col-span-3 md:col-span-2 col-span-1" v-if="loader">
     <LoaderComponent />
   </div>
-  <CardByIdComponent v-if="data" :data="data" />
+  <CardAsteroidByIdComponent v-if="data" :data="data" />
 </template>
 
 <script setup lang="ts">
 import { useFetchingStore } from '@/stores/fetching'
-import { defineProps, onMounted, ref, computed } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import LoaderComponent from '@/components/LoaderComponent.vue'
-import CardByIdComponent from '@/components/CardByIdComponent.vue'
+import CardAsteroidByIdComponent from '@/components/CardAsteroidByIdComponent.vue'
 
 const useFetching = useFetchingStore()
 const { asteroidById } = useFetching
